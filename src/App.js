48cfae8553061a3aa92b5b24a2b1eb1d42ee5ea7
XxHaRuToxX <youlayelvis@gmail.com>
2022-07-05 10:32:45 -0500

    respo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import GlobalStyle from './globalStyled';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { Home } from './pages/Home';
+import { PricingPage } from './pages/PricingPage';
+import { SignupPage } from './pages/SignupPage';
+
+export const App = () => {
+    return (
+        <>
+            <BrowserRouter>
+                <GlobalStyle />
+                <Routes>
+                    <Route path='/' element={<Home/>} />
+                    <Route path='/signup' element={<SignupPage/>} />
+                    <Route path='/pricing' element={<PricingPage/>} />
+                </Routes>
+            </BrowserRouter>
+        </>
+    );
 }
-
-export default App;
